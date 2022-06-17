61641556ae8684e7a41f8286bf412899c44df286
StuM <stum@datacom.co.nz>
2022-06-17 16:12:09 +1200

    quoting fixes and add example of global settings tag

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "resource_group_name" {
   description = "The resource group into which components are deployed"
 }
+
+variable "company" {
+  description = "The company name (used for resource tagging)"
+}
