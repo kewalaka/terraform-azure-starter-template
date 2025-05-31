8d0f0cc7273aa1bf4e531ede89252ccf56df729b
kewalaka <kewalaka@gmail.com>
2025-06-01 10:09:52 +1200

    fix: drop helpers and fix pipeline

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,18 +1,3 @@
-variable "appname" {
-  description = "Longer name of the application used for naming conventions."
-  type        = string
-}
-
-variable "short_appname" {
-  description = "Short name of the application used for naming conventions."
-  type        = string
-  validation {
-    # this is assuming that an environment name will be 4 characters or less - example for a storage account - st12345678901234prodae01
-    condition     = length(var.short_appname) <= 14
-    error_message = "Err: The short_appname should be 14 characters or less to fit within naming conventions for resources such as storage accounts."
-  }
-}
-
 variable "short_location_code" {
   description = "A short form of the location where resource are deployed, used in naming conventions."
   type        = string
